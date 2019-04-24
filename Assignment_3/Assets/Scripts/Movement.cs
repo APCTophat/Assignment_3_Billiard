3ee929368d387d4a8f108f1e09f35f90512a32a1
APCTophat <49924900+APCTophat@users.noreply.github.com>
2019-04-24 15:05:53 +0200

    prob final version

---
+++ Assignment_3/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D Rb2D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hdirect = Input.GetAxis("Horizontal");
+        float Vdirect = Input.GetAxis("Vertical");
+
+        Rb2D.velocity = new Vector2(Hdirect * 100, Vdirect * 100);         //gives the player movement
+      
+    }
+}
